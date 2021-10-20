7cbe4e42ee2f30c8714d8a289cfcfdb11788dee8
krishna pradhan <krishna.pradhan@telus.com>
2021-10-20 13:11:36 -0400

    cleaned up code

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, AsyncStorage } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, AsyncStorage } from "react-native";
 
-import { Countdown } from './src/components/Countdown';
-import { Focus } from './src/features/focus/Focus';
-import { Timer } from './src/features/timer/Timer';
-import { spacing } from './src/utils/sizes';
-import { colors } from './src/utils/colors';
-import { FocusHistory } from './src/features/focus/FocusHistory';
+import { Countdown } from "./src/components/Countdown";
+import { Focus } from "./src/features/focus/Focus";
+import { Timer } from "./src/features/timer/Timer";
+import { spacing } from "./src/utils/sizes";
+import { colors } from "./src/utils/colors";
+import { FocusHistory } from "./src/features/focus/FocusHistory";
 
 const STATUSES = {
   COMPLETE: 1,
@@ -18,11 +18,11 @@ export default function App() {
   const [focusHistory, setFocusHistory] = useState([]);
 
   const addFocusHistorySubjectWithState = (subject, status) => {
-    setFocusHistory([...focusHistory, { key: String(focusHistory.length + 1), subject, status }]);
+    setFocusHistory([
+      ...focusHistory,
+      { key: String(focusHistory.length + 1), subject, status },
+    ]);
   };
-
-  console.log(' focus hitory ', focusHistory);
-  console.log(' focus subject', focusSubject);
 
   const onClear = () => {
     setFocusHistory([]);
@@ -30,7 +30,7 @@ export default function App() {
 
   const saveFocusHistory = async () => {
     try {
-      await AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory));
+      await AsyncStorage.setItem("focusHistory", JSON.stringify(focusHistory));
     } catch (e) {
       console.log(e);
     }
@@ -38,7 +38,7 @@ export default function App() {
 
   const loadFocusHistory = async () => {
     try {
-      const history = await AsyncStorage.getItem('focusHistory');
+      const history = await AsyncStorage.getItem("focusHistory");
       if (history && JSON.parse(history).length) {
         setFocusHistory(JSON.parse(history));
       }
@@ -70,7 +70,7 @@ export default function App() {
           }}
         />
       ) : (
-        <View style={{ flex: 1}}>
+        <View style={{ flex: 1 }}>
           <Focus addSubject={setFocusSubject} />
           <FocusHistory focusHistory={focusHistory} onClear={onClear} />
         </View>
--- src/features/focus/FocusHistory.js
+++ src/features/focus/FocusHistory.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View, StyleSheet, FlatList, Text, SafeAreaView } from 'react-native';
+import React from "react";
+import { View, StyleSheet, FlatList, Text, SafeAreaView } from "react-native";
 
-import { fontSizes, spacing } from '../../utils/sizes';
+import { fontSizes, spacing } from "../../utils/sizes";
 import { colors } from "../../utils/colors";
-import { RoundedButton } from '../../components/RoundedButton';
+import { RoundedButton } from "../../components/RoundedButton";
 
 export const FocusHistory = ({ focusHistory, onClear }) => {
   const clearHistory = () => {
@@ -11,21 +11,19 @@ export const FocusHistory = ({ focusHistory, onClear }) => {
   };
 
   const HistoryItem = ({ item, index }) => {
-    console.log('item ', item);
     return <Text style={styles.historyItem(item.status)}>{item.subject}</Text>;
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+    <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
       {!!focusHistory.length && (
         <>
           <Text style={styles.title}>Things we've focused on</Text>
           <FlatList
             style={{ flex: 1 }}
-            contentContainerStyle={{ flex: 1, alignItems: 'center' }}
+            contentContainerStyle={{ flex: 1, alignItems: "center" }}
             data={focusHistory}
             renderItem={HistoryItem}
-            keyExtractor={(item) => item.id}
           />
           <View style={styles.clearContainer}>
             <RoundedButton size={75} title="Clear" onPress={() => onClear()} />
@@ -38,7 +36,7 @@ export const FocusHistory = ({ focusHistory, onClear }) => {
 
 const styles = StyleSheet.create({
   historyItem: (status) => ({
-    color: status > 1 ? 'red' : 'green',
+    color: status > 1 ? "red" : "green",
     fontSize: fontSizes.md,
   }),
   title: {
@@ -46,7 +44,7 @@ const styles = StyleSheet.create({
     fontSize: fontSizes.lg,
   },
   clearContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: spacing.md,
   },
 });
